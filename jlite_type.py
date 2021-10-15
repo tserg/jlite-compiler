ed1ab0e23458bf7a76673cf0c517c012bdf61a8a
Gary Tse <tse.rong.gary@gmail.com>
2021-10-15 22:35:13 +0800

    refactor typing into standalone file

---
+++ jlite_type.py
@@ -0,0 +1,71 @@
+from enum import Enum
+
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+class TypeCheckError(Exception):
+    """
+    Exception class for errors encountered while type checking.
+
+    ...
+
+    Attributes
+    ----------
+    expression: str
+    message: str
+
+    """
+    def __init__(
+        self,
+        expression: str,
+        additional_message: str=''
+    ) -> None:
+        self.expression = expression
+        self.message = "Invalid type: "
+        self.additional_message = additional_message
+
+    def __str__(self) -> str:
+
+        return f'{self.message} {self.expression}. {self.additional_message}'
+
+class BasicType(Enum):
+    """
+    Type class for basic types
+    """
+
+    INT = 'Int'
+    BOOL = 'Bool'
+    STRING = 'String'
+    VOID = 'Void'
+    OBJECT = 'Object'
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+class FunctionType():
+    """
+    Type class for function types
+
+    ...
+
+    Attributes
+    ----------
+    basic_type_list: List[Any]
+
+    """
+
+    # List of basic types that are taken as arguments
+    basic_type_list: List[Any]
+
+    def __init__(self, basic_type_list: List[Any]) -> None:
+        self.basic_type_list = basic_type_list
+
+TYPE_CONVERSION_DICT: Dict[str, "BasicType"] = {
+    'Int': BasicType.INT,
+    'Bool': BasicType.BOOL,
+    'String': BasicType.STRING,
+    'Void': BasicType.VOID,
+}
